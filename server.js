be6656ed0d5a6f8f646e342e9c7e1e17f4d9ed4a
Simone <ssimone.siepi@itis.pr.it>
2023-10-29 20:47:54 +0100

    test risposta server con pagina html

--- server.js
+++ server.js
@@ -3,37 +3,6 @@ const http=require("http");
 const url = require("url");
 const path= require("path");
 
-function validazioneForm(query, errors) {
-    const nome = query.nome;
-    const cognome = query.cognome;
-    const sesso = query.scelta;
-    const nascita = query.data_nascita;
-    const eta = query.eta;
-
-    if (!nome) {
-        errors.push("Il campo Nome e' obbligatorio.");
-    }
-
-    if (!cognome) {
-        errors.push("Il campo cognome e' obbligatorio.");
-    }
-
-    if (!sesso) {
-        errors.push("Il campo sesso e' obbligatorio.");
-    }
-
-    if (!nascita) {
-        errors.push("Il campo data di nascita e' obbligatorio.");
-    }
-
-    if (!eta) {
-        errors.push("Il campo eta' e' obbligatorio.");
-    } else if (isNaN(eta) || parseInt(eta) <= 0) {
-        errors.push('Il campo Eta deve essere un numero positivo.');
-    }
-}
-
-
 /* function requestHandler(request,response){
     let oggettoUrl=url.parse(request.url, "true");
     console.log("href"+oggettoUrl.href);//fa visuliazzare il nome completo
@@ -99,7 +68,8 @@ function validazioneForm(query, errors) {
  */
 
 function requestHandler(request,response){
-    let percorsoFile="."+req.url;
+    console.log(request.url);
+    let percorsoFile="."+request.url;
     if (percorsoFile==="./") {
         percorsoFile="./index.html";
     }
@@ -118,6 +88,115 @@ function requestHandler(request,response){
         case ".js":
             estensioneFile="text/javascript";
     }
+    
+    if (percorsoFile=="/recuperaDati") {
+        console.log("sono dentro!");
+        let messaggio = "";
+        let invia=false;
+        const query=oggettoUrl.query;
+        const nome=oggettoUrl.query.nome;
+        const cognome=oggettoUrl.query.cognome;
+        const sesso=oggettoUrl.query.scelta;
+        const nascita=oggettoUrl.query.data_nascita;
+        const eta=oggettoUrl.query.eta;
+        const info=oggettoUrl.query.info;
+        const regex = /^[a-zA-Z\s]+$/;//variabile di contorollo sui cui utilizzo una regular expression
+
+         let error=``;
+         if (!nome || !regex.test(nome)) {
+            invia=true;
+            error+=`
+            <label for="nome" class="error">Nome:</label><br>
+            <input type="text" id="nome" name="inserisci un nome"><br>
+            `
+         }
+
+         if (!cognome || !regex.test(cognome)) {
+            invia=true;
+            error+=`
+            <label for="cognome" class="error">Cognome:</label><br>
+            <input type="text" id="cognome" name="inserisci un cognome"><br>
+            `
+         }
+
+         if (!sesso) {
+            invia=true;
+            error+=`
+            <input type="radio" id="scelta" name="scelta" value="Uomo">
+            <label for="Uomo" class="error">Uomo</label><br>
+            <input type="radio" id="scelta" name="scelta" value="Donna">
+            <label for="Donna" class="error">Donna</label><br>
+            <input type="radio" id="scelta" name="scelta" value="non binario">
+            <label for="non_binario" class="error">Non binario</label><br>
+            `
+        }
+    
+        if (!nascita) {
+            invia=true;
+            error+=`
+            <label for="data_nascita" class="error">data di nascita:</label><br>
+            <input type="date" id="data_nascita" name="data_nascita"><br>
+            `
+        }
+    
+        if (!eta) {
+            invia=true;
+            error+=`
+            <label for="eta" class="error">Età</label><br>
+            <input type="number" id="eta" name="eta"><br>
+            `
+        } else if (isNaN(eta) || parseInt(eta) <= 0) {
+            invia=true;
+            error+=`
+            <label for="eta" class="error">Età</label><br>
+            <input type="number" id="eta" name="eta"><br>
+            `  
+        }
+
+        messaggio=`<!DOCTYPE html>
+        <html lang="it">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="css/Style.css">
+            <title>form completo in node</title>
+        </head>
+        <body>
+            <form action="recuperaDati" method="get">
+               
+                ${error}
+
+                <input type="submit">
+        
+            </form>
+        </body>
+        </html> `
+
+        if (invia==true) {
+            response.writeHead(200,{'Content-Type': 'text/html'});
+            console.log("funziona!");
+            response.write(messaggio);
+            response.end();
+        }
+        else{
+            response.writeHead(200,{'Content-Type': 'text/plain'});
+            response.write("form compilato correttamente");
+            response.end();
+        }
+
+            
+    }
+
+    fs.readFile(percorsoFile,function(error,data){
+        if (error) {
+            response.writeHead(404);
+        }else{
+            response.writeHead(200,{"content-Type":estensioneFile});
+            response.write(data,"utf8");
+        }
+        response.end(); 
+    });
+
 }
 
 const server=http.createServer(requestHandler);
